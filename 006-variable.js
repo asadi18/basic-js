95bf479e7c0b65cee6d5560556563950227a34af
asadi18 <asadishteaque@gmail.com>
2022-07-09 07:11:04 +0600

    Till 7th lesson

---
+++ 006-variable.js
@@ -0,0 +1,6 @@
+var name = 'Asad'
+var age = 24
+console.log(name)
+console.log (age)
+console.log (name + 'Learning java script')
+console.log('he started learning at 2022 at the age of '+age)
--- Hello.js
+++ Hello.js
@@ -1,1 +1,6 @@
 console.log('ALLAH TAKE CARE OF US AND GIVE US EVERYTHING WE NEED')
+console.log(25)
+console.log('Anything will not work without saving the file')
+console.log(56.65)//We can print float directly
+console.log('Asad Ishteaque age '+24)
+console.log(9+9)
