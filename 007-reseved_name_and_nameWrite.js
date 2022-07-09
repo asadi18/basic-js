405d4676de447599b6d6e3d28bf89a6747949984
asadi18 <asadishteaque@gmail.com>
2022-07-09 07:42:20 +0600

    4 files now

---
+++ 007-reseved_name_and_nameWrite.js
@@ -0,0 +1,11 @@
+// There has a lot of reserved name we cant use as variable. 
+
+//Name can be written as 
+var accountName = 'Asad Ishteaque' 
+
+//This is most common in js but we can also use
+
+var Account_Number ='5121698416658'
+
+console.log (accountName)
+console.log (Account_Number)
