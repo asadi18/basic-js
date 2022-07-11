38103d18cba7ca555bea3174ddd52469a7b6e78c
asadi18 <asadishteaque@gmail.com>
2022-07-11 20:33:40 +0600

    till 10

---
+++ 010-stringInJavaScript.js
@@ -0,0 +1,10 @@
+//we will learn the basics of string 
+var a = 'String'
+var b = "under double"
+var c = `above tab key`
+
+var d =String ('asad')
+var e = String (154)
+var f =String (3.1414)
+
+console.log(a,b,c,d,e,f)
