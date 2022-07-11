f655e9dd1c75a35c4e18074c6b3f6bf9770b8458
asadi18 <asadishteaque@gmail.com>
2022-07-11 20:11:43 +0600

    Till lesson 9

---
+++ 009-All-about-numbers.js
@@ -0,0 +1,24 @@
+//Integer data type 
+var n=45
+console.log (n)
+
+//floating data type
+var f =36.64
+console.log (f)
+
+//for parsing 
+var nn=Number (56.87)
+
+console.log (Number.parseInt(nn)) // we can also use "console.log (Number.parseFloat(nn))"
+
+// for checking maximum and minimum valu 
+
+console.log (Number.MAX_VALUE)
+console.log (Number.MIN_VALUE)
+
+//Infinity Number data type 
+
+console.log (1/0)
+
+// NAN (not a number data type)
+console.log('abd'*50)
