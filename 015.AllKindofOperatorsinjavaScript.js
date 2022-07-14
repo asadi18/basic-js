aa479887ccbc4d2f19523e5071b685e5bd97fdc6
asadi18 <asadishteaque@gmail.com>
2022-07-14 20:17:43 +0600

    all kind of operators

---
+++ 015.AllKindofOperatorsinjavaScript.js
@@ -0,0 +1,37 @@
+// WE will learn most of the data types in this lesson
+//1. Arithmetic operator 
+
+/*var a = 11
+var b = 6
+console.log(a % b)
+console.log(a++)
+console.log(b--)
+console.log(++a)
+console.log(--b)*/
+
+//2.Assignment Operator
+var x=10
+var y= 20
+x+=y // alos try - and multpy and devide */% modulas also
+console.log(x)
+
+// Logical Operator 
+/* 1. AND - && operator 
+2. OR- || operator
+3. Not - operator !*/
+
+//4. comparison Operator 
+var a =10 // try a =20 it will true 
+var b=20
+console.log(a==b)
+
+// try alos not equal != , >, less and less equal etc. 
+
+console.log (a!=b)// (a>b),(a<b),a>=b,a<=b
+
+// it can also comparae string and integer like 
+
+var j='50'
+var i = 50
+console.log (j==i) // it will compare the string as number. for filter this usee triple = then it will also compare data type
+console.log (j===i)// returned false
