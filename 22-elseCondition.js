00022bd857e028b750ebf3d2f91cc30ea5ce184f
asadi18 <asadishteaque@gmail.com>
2022-07-16 18:32:33 +0600

    Else Condition

---
+++ 22-elseCondition.js
@@ -0,0 +1,23 @@
+// all same as if condition just simplyfy the code 
+
+var a=34 // change the value for checking
+var b=45 
+
+if (a>b){
+    console.log ('A is greater than B')
+}
+
+else{
+    console.log ('B is greater than A')
+}
+
+//For odd and even number
+
+var n = 9 // change the value for checking like enter 50, or any even number
+if (n%2==0){
+    console.log(n + 'is even Numeber ')
+}
+
+else{
+    console.log(n + 'is odd Numeber ')
+}
