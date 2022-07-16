6549a0c24f4b24c7dee8d2142410f1d170d39bc5
asadi18 <asadishteaque@gmail.com>
2022-07-16 18:27:50 +0600

    If condition use

---
+++ 021-useofIfStatement.js
@@ -0,0 +1,23 @@
+// we need it when we need to know true false etc for 2 oprerands
+
+var a=34 // change the value for checking
+var b=45 
+
+if (a>b){
+    console.log ('A is greater than B')
+}
+
+if (a<b){
+    console.log ('B is greater than A')
+}
+
+//For odd and even number
+
+var n = 9 // change the value for checking like enter 50, or any even number
+if (n%2==0){
+    console.log(n + 'is even Numeber ')
+}
+
+if (n%2==1){
+    console.log(n + 'is odd Numeber ')
+}
