1b9449ef28cfa64e24f38366fdeb89a69ae43ae5
asadi18 <asadishteaque@gmail.com>
2022-07-16 18:17:45 +0600

    logic 20

---
+++ 020-LogicandCondition.js
@@ -0,0 +1,9 @@
+// All theory for this small video 
+// Basically described the true false etc. 
+
+// 4 type of conditions here 
+/*
+1. if condition
+2. Else condition
+3. Elseif condition
+4. Switch Statement */
