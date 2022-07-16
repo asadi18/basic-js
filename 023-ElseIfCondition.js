13a68d483dc427d37a85e2735115cf0fc5a107cc
asadi18 <asadishteaque@gmail.com>
2022-07-16 18:44:42 +0600

    Else if condition clear now

---
+++ 023-ElseIfCondition.js
@@ -0,0 +1,34 @@
+// it will add multiple features
+
+var a=20// change the value for checking
+var b=20 
+
+if (a>b){
+    console.log ('A is greater than B')
+}
+
+else if (a<b){
+    console.log ('B is greater than A')
+}
+
+else {
+    console.log ('Both are equal')
+}
+
+
+//For odd and even number
+
+var n = 0 // change the value for checking like enter 50, or any even number
+
+
+if (n==0){
+    console.log( n + 'is Zero ')
+}
+
+else if (n%2==0){
+    console.log(n + 'is even Numeber ')
+}
+
+else{
+    console.log(n + 'is odd Numeber ')
+}
