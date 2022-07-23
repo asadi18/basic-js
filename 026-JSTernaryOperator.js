294d992933ab357a2932d9af3a9e0713ba919d12
asadi18 <asadishteaque@gmail.com>
2022-07-23 16:39:45 +0600

    Ternary operator Concept

---
+++ 026-JSTernaryOperator.js
@@ -0,0 +1,20 @@
+//Before we were writing codes for if and else with a lot of lines. 
+//example - to check add even 
+
+/*
+var n=10
+var str= ''
+if (n%2==0){
+    str ='EVEN'
+}
+    else {
+        str ='ODD'
+    }
+console.log(str)    
+*/
+
+//But with Ternary Operator
+
+var n=10
+var result =n%2 ==0 ? 'EVEN' : 'ODD' // All condition in one line 
+console.log (result) // condition ? true side : False 
