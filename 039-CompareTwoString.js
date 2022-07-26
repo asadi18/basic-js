11217435da18b4a77d97ae44320bd00193cc415c
asadi18 <asadishteaque@gmail.com>
2022-07-26 15:20:46 +0600

    String Compare

---
+++ 039-CompareTwoString.js
@@ -0,0 +1,12 @@
+// In thsi program we will see how to compare two string
+
+let a = 'abc'
+let b = 'bcd'
+console.log(a<b) // also try (a>b) and (a==b)
+
+let x='A'
+let y='a'
+console.log (x>y) // also try (x>y) and (x==y)
+
+// It converts a to a number
+console.log('001'==1)
