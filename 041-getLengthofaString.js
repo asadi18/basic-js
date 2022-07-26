34c25fd7916f4286259b5c57d9c3e81463ba165b
asadi18 <asadishteaque@gmail.com>
2022-07-26 16:32:59 +0600

    String Length

---
+++ 041-getLengthofaString.js
@@ -0,0 +1,17 @@
+// we will check the lenght of string simple code 
+
+let str2 ='Asad Ishteaque'
+console.log(str2.length)
+
+// we will check the lenght of string Loop code 
+let str ='Asad Ishteaque'
+let length=0
+while (true){
+if (str.charAt(length)==''){
+    break
+}
+else {
+    length++
+}
+}
+console.log(length)
