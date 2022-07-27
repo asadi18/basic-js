7540e2df7254795476ef0d69cb698b750a9b890b
asadi18 <asadishteaque@gmail.com>
2022-07-27 15:23:31 +0600

    Array Declaration

---
+++ 042-WhatisArray.js
@@ -0,0 +1,1 @@
+// this lesson only cover the theory of Array 
---
+++ 043-howtoDeclareArray.js
@@ -0,0 +1,16 @@
+// This lesson covers how to Declare Array 
+
+let arr = [1,2,3,4,5,6]
+//console.log(arr[3]) // it will count from 0 and this output will be 4
+arr [10] = 11
+console.log(arr.length)
+// console.log(arr)    // try this line for seeing how much it is printing 
+// it will define some empty arrays because we havent defined them
+
+// To replace any array then 
+arr [3]=35
+console.log(arr)
+
+// to declare array with constractor
+let arr2 = Array[1,3,4]
+console.log(arr2)    // previous one is easy and mostly used
