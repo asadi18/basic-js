0920031c92cbfb7807054f1051b97bcd108d6856
asadi18 <asadishteaque@gmail.com>
2022-07-30 19:32:11 +0600

    changing any object

---
+++ 053-settingObjectProperties.js
@@ -0,0 +1,18 @@
+// to replace any value 
+
+let obj = {
+    x:35,
+    y:43,
+    z:65
+}
+obj.x =16
+//console.log(obj)
+
+// if the value we are changing is not exist then no value will show
+ // another way ]
+ obj ['y'] = 100
+ // another way to cheange
+ let prop = 'z'
+ obj [prop]=55
+ console.log(obj)
+
