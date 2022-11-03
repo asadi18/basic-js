09596c0414a2cf79721ea9bd99cc1828c5af4b01
asadi18 <asadishteaque@gmail.com>
2022-11-03 15:55:08 +0600

    This is Currying Function

--- 081-CurryinginJS.js
+++ 081-CurryinginJS.js
@@ -3,3 +3,17 @@ function add(a,b,c){
     return a + b + c 
 }
 console.log(add (4,5,6))
+
+// For CUrrying 
+
+function currying (a){
+    return function (b){
+        return function (c){
+            return a + b + c
+        }
+    }
+    
+}
+
+var result = currying (5)(10)(15)
+console.log(result)
