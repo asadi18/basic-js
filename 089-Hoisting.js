98f06fe5ad8057fbc2512e3686a282cebac1921d
asadi18 <asadishteaque@gmail.com>
2022-11-05 12:49:06 +0600

    This is hoisting

---
+++ 089-Hoisting.js
@@ -0,0 +1,24 @@
+var a = 100
+
+// newPrint(a)
+
+print (10)
+
+var newPrint = print
+newPrint(45)
+
+function print (a){
+    console.log(a)
+}
+
+print(a)
+
+// Creational phase 
+// a = undefined
+// newPrint = undefined 
+// print = ref
+
+
+// Executional phase 
+// a = 100 
+// newPrint = ref
