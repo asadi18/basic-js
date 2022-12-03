68ea253ccd3c99390817f31fb32ba26c85b9deec
asadi18 <asadishteaque@gmail.com>
2022-12-03 18:13:12 +0600

    properties and methods 110 to 115

--- index.js
+++ index.js
@@ -93,11 +93,11 @@ return {
 }
 }
 
-var rect1 = createRect(10, 20)
-rect1.draw()
+// var rect1 = createRect(10, 20)
+// rect1.draw()
 
-var rect2 = createRect (4,5)
-rect2.draw()
+// var rect2 = createRect (4,5)
+// rect2.draw()
 
 
 
@@ -123,6 +123,95 @@ var Rectangle = function(width,height){
     }
 }
 
-var rect3 = new Rectangle(10,8)
-rect3.draw()
+// var rect3 = new Rectangle(10,8)
+// rect3.draw()
 
+
+
+// 111 ----------- New Keyword  -----------------
+
+
+function myNew(constructor){
+    var obj = {}
+    Object.setPrototypeOf(obj,constructor.prototype)
+    var argsArray = Array.prototype.slice.apply(arguments)
+    constructor.apply(obj,argsArray.slice(1))
+
+    return obj
+}
+var rect4 = myNew (Rectangle,10,8)
+rect4.draw()
+
+
+//----------------END --------------------------
+
+
+
+
+// ------ 112  Factory vs Constructor --------------
+
+// All thory about this 
+
+
+// ---------------- END -------------------------
+
+
+
+
+//113------- Constructor Property--------
+
+
+
+var str = new String('Hey')
+console.log('This is our string '+ str)
+
+
+
+//-----------------END------------------
+
+
+//114------------- Functions Are Object --------------------
+
+function test (){
+    console.log('Something')
+}
+console.log(test.name, test.length)
+
+var Rect = new Function('width', 'height', `this.width = width
+this.height = height
+
+this.draw = function(){
+    console.log('I am rectangle')
+    console.log('My width is '+ this.width)
+    console.log('My height is '+ this.height)
+}
+this.heightprintProperties = function(){
+    console.log('My width is '+ this.width)
+    console.log('My height is '+ this.height)
+}`)
+
+var rect5 = new Rect(4,5)
+console.log(rect5)
+
+
+//-----------------------END--------------------
+
+
+
+
+// -------------115 Bind, Call, Apply ---------------
+
+
+
+function myFunc(c,d){
+    console.log(this)
+    console.log(this.a + this.b + c + d)
+}
+
+myFunc.call({a:40, b:25},10,5) // for call and apply
+myFunc.apply({a:40, b:25}[10,5])
+
+// For bind 
+
+var testBind = myFunc.bind({a:7, b:3})
+testBind(5,7)
